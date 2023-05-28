80ecdfa7c232a0a2c88da571732b01378f6e917f
pheonix2121 <bobbypanwar73@gmail.com>
2023-05-29 02:32:53 +0530

    task 14

--- project/src/Components/Expns.js
+++ project/src/Components/Expns.js
@@ -1,29 +1,53 @@
-import React, { useState } from 'react';
-
-
+import React, { useState, useEffect } from 'react';
+import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './NewExpense/ExpensesFilter';
-import ExpensesList from './ExpensesList';
 import './Expns.css';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear]=useState('2020');
+  const [expenses, setExpenses] = useState(props.items);
+  const [filteredYear, setFilteredYear] = useState('2020');
 
-  const filterChangeHandler = selectedYear => {
+  useEffect(() => {
+    setExpenses(props.items);
+  }, [props.items]);
+
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter((expense) => {
+  const deleteExpenseHandler = (id) => {
+    setExpenses((prevExpenses) => {
+      const updatedExpenses = prevExpenses.filter((expense) => expense.id !== id);
+      return updatedExpenses;
+    });
+  };
+
+  const filteredExpenses = expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  
+  let expensesContent = <p>"Only single Expense here. Please add more..."</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        id={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        onDelete={deleteExpenseHandler}
+      />
+    ));
+  }
 
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      
-      <ExpensesList items={filteredExpenses} />
+      <ExpensesChart expenses={filteredExpenses} />
+      {expensesContent}
     </Card>
   );
 };
